204f32b9f6394ec0202b9aafaa8a7f3627b04df4
Ivan da Silva Rosa <iivan.si@msn.com>
2021-03-08 20:22:54 -0300

    finalização do Collapsable Aside Navigation

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+/* generic function for TOGGLE a class  */
+function toggleClass(elem, nameClass) {
+    document.querySelector(elem).classList.toggle(nameClass);
+}
+
+/* generic function for ADD a class  */
+function addClass(elem, nameClass) {
+    document.querySelector(elem).classList.add(nameClass);
+}
+
+/* generic function for REMOVE a class  */
+function removeClass(elem, nameClass) {
+    document.querySelector(elem).classList.remove(nameClass);
+}
+/* function to ADD a CLICK event listener to the SEARCH BUTTON */
+const faSearch = document.querySelector('.fa-search');
+faSearch.addEventListener('click', () => {
+    toggleClass('.search', 'top100PercOp1');
+    removeClass('.navigation', 'navigationOpen');
+});
+
+/* function to ADD a CLICK event listener to the MENU BUTTON */
+const faBars = document.querySelector('.fa-bars');
+faBars.addEventListener('click', () => {
+    toggleClass('.navigation', 'navigationOpen');
+    removeClass('.search', 'top100PercOp1');
+});
